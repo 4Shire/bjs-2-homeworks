a005344426fb2be008c380d1b5b7b3af5e4a835e
4Shire <t-grishe@yandex.ru>
2022-10-08 15:10:57 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);  
 
-  return result; // boolean
+  return result; 
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(e => e > 0).filter(e => e % 3 ===0).map(item => (item * 10));
 
-  return resultArr; // array
+  return resultArr; 
 }
